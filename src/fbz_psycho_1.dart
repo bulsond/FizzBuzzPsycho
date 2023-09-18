f91487b7f476edebb278809f587e546f930f30b6
bulsond <bulsond@gmail.com>
2023-09-18 09:49:04 +0300

    feat: adds a functional variant

---
+++ src/fbz_psycho_1.dart
@@ -0,0 +1,12 @@
+void main(List<String> args) {
+  for (var i = 0; i < 31; i++) {
+    outputs.entries.firstWhere((e) => e.key(i)).value(i);
+  }
+}
+
+final outputs = <bool Function(int value), void Function(int value)>{
+  ((v) => v % 15 == 0): (v) => print('FizzBuzz,'),
+  ((v) => v % 3 == 0): (v) => print('Fizz,'),
+  ((v) => v % 5 == 0): (v) => print('Buzz,'),
+  ((v) => true): (v) => print('${v},'),
+};
