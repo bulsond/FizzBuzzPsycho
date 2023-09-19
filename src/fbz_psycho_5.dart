5e5cbae6226d05b829992683ac2e50724146aa5f
bulsond <bulsond@gmail.com>
2023-09-19 17:45:35 +0300

    feat: adds a variant wtih the decorator pattern

---
+++ src/fbz_psycho_5.dart
@@ -0,0 +1,104 @@
+// <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+/// Decorator Pattern
+void main(List<String> args) {
+  final fbOutput = FizzBuzzOutput(
+    outputCapable: BuzzCapable(
+      outputCapable: FizzCapable(
+        outputCapable: FizzBuzzCapable(
+          outputCapable: DeadCapable(),
+        ),
+      ),
+    ),
+  );
+
+  for (var i = 0; i < 31; i++) {
+    final output = fbOutput.getFor(number: i);
+    print(output);
+  }
+}
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+class FizzBuzzOutput {
+  const FizzBuzzOutput({required this.outputCapable});
+
+  final OutputCapable outputCapable;
+
+  String getFor({required int number}) {
+    final text = outputCapable.getOutputText(number: number);
+    if (text.isEmpty) {
+      return '$number,';
+    }
+    return text;
+  }
+}
+
+abstract class OutputCapable {
+  const OutputCapable();
+
+  OutputCapable get outputCapable;
+  int get divisor;
+  String get message;
+
+  String getOutputText({required int number}) {
+    final text = outputCapable.getOutputText(number: number);
+    if (text.isNotEmpty) {
+      return text;
+    }
+    if (number % divisor == 0) {
+      return message;
+    }
+    return '';
+  }
+}
+
+class BuzzCapable extends OutputCapable {
+  const BuzzCapable({required this.outputCapable});
+
+  final OutputCapable outputCapable;
+
+  @override
+  int get divisor => 5;
+
+  @override
+  String get message => 'Buzz,';
+}
+
+class FizzCapable extends OutputCapable {
+  const FizzCapable({required this.outputCapable});
+
+  final OutputCapable outputCapable;
+
+  @override
+  int get divisor => 3;
+
+  @override
+  String get message => 'Fizz,';
+}
+
+class FizzBuzzCapable extends OutputCapable {
+  const FizzBuzzCapable({required this.outputCapable});
+
+  final OutputCapable outputCapable;
+
+  @override
+  int get divisor => 15;
+
+  @override
+  String get message => 'FizzBuzz,';
+}
+
+class DeadCapable extends OutputCapable {
+  const DeadCapable();
+
+  @override
+  OutputCapable get outputCapable => throw UnimplementedError();
+
+  @override
+  int get divisor => throw UnimplementedError();
+
+  @override
+  String get message => throw UnimplementedError();
+
+  @override
+  String getOutputText({required int number}) => '';
+}
