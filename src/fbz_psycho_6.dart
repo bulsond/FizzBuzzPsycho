c826eabbdb62d0c827931c15e0f86a3a48c5f822
bulsond <bulsond@gmail.com>
2023-09-19 23:55:21 +0300

    feat: adds a variant with the Chain of Resp

---
+++ src/fbz_psycho_6.dart
@@ -0,0 +1,88 @@
+// <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+/// Chain of Responsibility Pattern
+void main(List<String> args) {
+  final output = FizzBuzzOutput(
+    handler: FizzBuzzHandler(
+      next: FizzHandler(
+        next: BuzzHandler(
+          next: NumberHandler(next: null),
+        ),
+      ),
+    ),
+  );
+  for (var i = 0; i < 31; i++) {
+    output.printFor(number: i);
+  }
+}
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+class FizzBuzzOutput {
+  const FizzBuzzOutput({required this.handler});
+
+  final Handler handler;
+
+  void printFor({required int number}) {
+    handler.handle(number: number);
+  }
+}
+
+abstract class Handler {
+  const Handler({required this.next});
+
+  final Handler? next;
+
+  int get divisor;
+  String get message;
+
+  void handle({required int number}) {
+    if (message.isEmpty) {
+      print(number);
+      return;
+    }
+    if (number % divisor == 0) {
+      print(message);
+      return;
+    }
+    next?.handle(number: number);
+  }
+}
+
+class FizzBuzzHandler extends Handler {
+  const FizzBuzzHandler({required super.next});
+
+  @override
+  int get divisor => 15;
+
+  @override
+  String get message => 'FizzBuzz,';
+}
+
+class FizzHandler extends Handler {
+  const FizzHandler({required super.next});
+
+  @override
+  int get divisor => 3;
+
+  @override
+  String get message => 'Fizz,';
+}
+
+class BuzzHandler extends Handler {
+  const BuzzHandler({required super.next});
+
+  @override
+  int get divisor => 5;
+
+  @override
+  String get message => 'Buzz,';
+}
+
+class NumberHandler extends Handler {
+  const NumberHandler({required super.next});
+
+  @override
+  int get divisor => 1;
+
+  @override
+  String get message => '';
+}
